251f8110c81878471f889973e583ed2e8a16210d
r0m3c <leandrogamarra@rocketmail.com>
2021-03-02 19:32:06 +0000

    d

--- main.cpp
+++ main.cpp
@@ -7,6 +7,46 @@
 // Put the assignment code here
 
 class Quaternion {
+  public: 
+    double a, b, c, d;
+
+    Quaternion(double p, double q, double r, double s) {
+      a = p;
+      b = q;
+      c = r;
+      d = s;
+  }
+    bool operator==(const Quaternion p) {
+      if ((p->a==this->a)&&(p->b==this->b)&&(p->c==this->c)&&(p->d==this->d)) {
+        return true;
+      }
+      else if {
+        return false;
+  }
+    Quaternion operator+(const Quaternion p) {
+      Quaternion r;
+      r.a=p.a+this->a;
+      r.b=p.b+this->b;
+      r.c=p.c+this->c;
+      r.d=p.d+this->d;
+      return r;
+  }
+    Quaternion operator-(const Quaternion p) {
+      Quaternion r;
+      r.a=p.a-this->a;
+      r.b=p.b-this->b;
+      r.c=p.c-this->c;
+      r.d=p.d-this->d;
+      return r;
+  }
+    Quaternion operator*(double s) {
+      Quaternion p;
+      p.a=s*this->a;
+      p.b=s*this->b;
+      p.c=s*this->c;
+      p.d=s*this->d;
+      return p;
+  }
 };
 
 //------------------------------
